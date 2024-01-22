eb5bd0cd7b09f7bd8f7f85565ea2ac74347222d1
Lekuru <contact@lekuru.xyz>
2024-01-22 19:48:57 +0100

    Add framework base actions

---
+++ app/handlers/framework/__init__.py
@@ -0,0 +1,12 @@
+
+from app.engine import Penguin, Instance
+
+from . import *
+
+@Instance.events.register('payloadBILogAction')
+def action(client: Penguin, json: dict):
+    Instance.actions.call(json['action'], client, json)
+
+@Instance.events.register("/framework")
+def framework(client: Penguin, json: dict):
+    Instance.triggers.call(json['triggerName'], client, json)
