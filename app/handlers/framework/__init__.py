54de852e560dfed7b8e89a42a6169588045b2569
Lekuru <contact@lekuru.xyz>
2024-01-22 23:49:33 +0100

    Fix `payloadBILogAction` trigger

--- app/handlers/framework/__init__.py
+++ app/handlers/framework/__init__.py
@@ -3,10 +3,10 @@ from app.engine import Penguin, Instance
 
 from . import game, windowmanager
 
-@Instance.events.register('payloadBILogAction')
-def action(client: Penguin, json: dict):
-    Instance.actions.call(json['action'], client, json)
-
 @Instance.events.register("/framework")
 def framework(client: Penguin, json: dict):
     Instance.triggers.call(json['triggerName'], client, json)
+
+@Instance.triggers.register('payloadBILogAction')
+def action(client: Penguin, json: dict):
+    Instance.actions.call(json['action'], client, json)
