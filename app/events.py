d1b01cac4df94d605479ba8ee1b1349dcac43299
Lekuru <contact@lekuru.xyz>
2024-01-22 19:44:04 +0100

    Add actions & trigger classes

--- app/events.py
+++ app/events.py
@@ -1,5 +1,7 @@
 
-from typing import Callable, Dict
+from typing import Callable, Dict, List
+from app import engine
+
 import logging
 
 class EventHandler:
@@ -7,7 +9,7 @@ class EventHandler:
         self.handlers: Dict[str, Callable] = {}
         self.logger = logging.getLogger("events")
 
-    def call(self, client, type, args) -> None:
+    def call(self, client: engine.Penguin, type: str, args: List[str]) -> None:
         self.logger.debug(f'Called "{type}": {args}')
 
         if type in self.handlers:
@@ -28,5 +30,39 @@ class EventHandler:
 
             self.handlers[type] = handler
             self.logger.info(f'Registered event: "{type}"')
+            return handler
+        return wrapper
+
+class TriggerHandler(EventHandler):
+    def __init__(self) -> None:
+        self.handlers: Dict[str, Callable] = {}
+        self.logger = logging.getLogger("triggers")
+
+    def call(self, trigger: str, client: engine.Penguin, json: dict) -> None:
+        if not client.window_manager.loaded:
+            return
+        return super().call(client, trigger, [json])
+
+    def register(self, trigger: str):
+        def wrapper(handler: Callable):
+            self.handlers[trigger] = handler
+            self.logger.info(f"Registered trigger: {trigger}")
+            return handler
+        return wrapper
+
+class ActionHandler(EventHandler):
+    def __init__(self) -> None:
+        self.handlers: Dict[str, Callable] = {}
+        self.logger = logging.getLogger("actions")
+
+    def call(self, action: str, client: engine.Penguin, json: dict) -> None:
+        if not client.window_manager.loaded:
+            return
+        return super().call(client, action, [json])
+
+    def register(self, action: str):
+        def wrapper(handler: Callable):
+            self.handlers[action] = handler
+            self.logger.info(f"Registered action: {action}")
             return handler
         return wrapper
