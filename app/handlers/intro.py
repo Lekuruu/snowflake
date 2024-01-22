4045744ffa28730bf61c8ad36dc117e83f8cf535
Lekuru <contact@lekuru.xyz>
2024-01-22 20:02:20 +0100

    Add base `/ready` handler

---
+++ app/handlers/intro.py
@@ -0,0 +1,17 @@
+
+from ..engine import Instance, Penguin
+
+@Instance.events.register('/ready')
+def ready_handler(client: Penguin):
+    # Initialize window manager
+    client.window_manager.load()
+
+    # Needs to be sent to show the player select
+    client.send_tag('P_MAPBLOCK', 't', 1, 1, 'iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAADklEQVQImWNghgEGIlkADWEAiDEh28IAAAAASUVORK5CYII=')
+    client.send_tag('P_MAPBLOCK', 'h', 1, 1, 'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAGCAAAAADfm1AaAAAADklEQVQImWOohwMG8pgA1rMdxRJRFewAAAAASUVORK5CYII=')
+
+    # Align player select to center
+    client.send_tag('UI_ALIGN', 101, 0, 0, 'center', 'scale_none')
+    client.send_tag('W_PLACE', 0, 1, 0)
+
+    # TODO: ...
