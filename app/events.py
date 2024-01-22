abf164ed81a1762c45c357fa08f7c5d2fa7a1531
Lekuru <contact@lekuru.xyz>
2024-01-22 18:57:26 +0100

    Refactor event handler

--- app/events.py
+++ app/events.py
@@ -1,11 +1,10 @@
 
-from typing import Callable, Dict, List
-from collections import defaultdict
+from typing import Callable, Dict
 import logging
 
 class EventHandler:
     def __init__(self) -> None:
-        self.handlers: Dict[str, List[Callable]] = defaultdict(list)
+        self.handlers: Dict[str, Callable] = {}
         self.logger = logging.getLogger("events")
 
     def call(self, client, type, args) -> None:
@@ -14,18 +13,16 @@ class EventHandler:
             for handler in self.handlers[type]:
                 handler(client, *args)
 
-    def delete(self, type):
-        self.logger.debug(f'Deleting "{type}"...')
-        if type in self.handlers:
-            try:
-                del self.handlers[type]
-            except Exception as e:
-                self.logger.error(f'Failed to delete "{type}": {e}')
-
-    def register(self, type: str) -> Callable:
+    def register(self, type: str, login_required: bool = True) -> Callable:
         def wrapper(handler: Callable) -> Callable:
-            if type in self.handlers:
-                self.handlers[type].append(handler)
-            self.logger.info(f"Registered event: {type}")
+            def login_wrapper(client, *args):
+                if not client.logged_in: return
+                handler(client, *args)
+
+            if login_required:
+                handler = login_wrapper
+
+            self.handlers[type] = handler
+            self.logger.info(f'Registered event: "{type}"')
             return handler
         return wrapper
