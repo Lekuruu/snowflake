f84f4913f7cdc2cc9609c06708b5302800e8c145
Lekuru <contact@lekuru.xyz>
2024-01-23 17:39:39 +0100

    Implement matchmaking calls

--- app/handlers/framework/matchmaking.py
+++ app/handlers/framework/matchmaking.py
@@ -16,11 +16,8 @@ def on_element_selected(client: Penguin, data: dict):
     client.power = GameData['Ninjas'][client.element]['Attack']
     client.move = GameData['Ninjas'][client.element]['Move']
 
-    client.logger.info(f'Joined matchmaking queue with "{client.element}"')
-
-    # TODO: Handle matchmaking
+    client.server.matchmaking.add(client)
 
 @Instance.triggers.register('mmCancel')
 def on_matchmaking_cancel(client: Penguin, data: dict):
-    client.logger.info('Left matchmaking queue')
-    # TODO: Handle matchmaking
+    client.server.matchmaking.remove(client)
