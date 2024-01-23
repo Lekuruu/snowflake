f3caccbd05e51a7f31615eb25bf44b28c241e248
Lekuru <contact@lekuru.xyz>
2024-01-23 19:13:53 +0100

    Add base game class

---
+++ app/engine/game.py
@@ -0,0 +1,34 @@
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .penguin import Penguin
+
+import random
+
+class Game:
+    def __init__(self, fire: "Penguin", snow: "Penguin", water: "Penguin") -> None:
+        self.fire = fire
+        self.snow = snow
+        self.water = water
+
+        self.round = 1
+        self.enemies = []
+        self.map = random.randrange(1, 3)
+
+    @property
+    def clients(self) -> list["Penguin"]:
+        return [self.fire, self.snow, self.water]
+
+    def start(self) -> None:
+        self.fire.game = self
+        self.snow.game = self
+        self.water.game = self
+        self.wait_for_players()
+        # TODO: ...
+
+    def wait_for_players(self) -> None:
+        """Wait for all players to finish loading the game"""
+        for player in self.clients:
+            while not player.in_game:
+                pass
