487c70da8d0f9a307de2b89b7701419064805e56
Lekuru <contact@lekuru.xyz>
2024-01-24 22:52:19 +0100

    Add `add_sound` function

--- app/objects/gameobject.py
+++ app/objects/gameobject.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from .collections import SoundCollection
 from ..engine import Instance
 from .asset import Asset
+from .sound import Sound
 
 if TYPE_CHECKING:
     from ..engine.game import Game
@@ -38,6 +39,24 @@ class GameObject:
             0,  # TODO
             1,  # TODO
             0   # TODO
+        )
+
+    def add_sound(
+        self,
+        name: str,
+        looping: bool = False,
+        volume: int = 100,
+        radius: int = 0
+    ) -> None:
+        self.sounds.add(
+            Sound.from_name(
+                name,
+                looping,
+                volume,
+                radius,
+                self.id,
+                self.id # TODO: Different id for response object?
+            )
         )
 
     def play_sound(self, sound_name: str) -> None:
