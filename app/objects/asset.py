6f1173581089a8f648a4ca7d434d410e8ee2b64c
Lekuru <contact@lekuru.xyz>
2024-01-24 23:29:14 +0100

    Make `Asset` hashable

--- app/objects/asset.py
+++ app/objects/asset.py
@@ -6,3 +6,9 @@ class Asset:
     index: int
     name: str
     url: str
+
+    def __eq__(self, asset: "Asset") -> bool:
+        return self.index == asset.index
+
+    def __hash__(self) -> int:
+        return hash(self.index)
