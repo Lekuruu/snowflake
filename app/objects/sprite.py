1ef1fdde50df29c8a047f9169119f8641aaadb9a
Lekuru <contact@lekuru.xyz>
2024-01-24 22:58:01 +0100

    Implement sprite loading methods

--- app/objects/sprite.py
+++ app/objects/sprite.py
@@ -1,7 +1,17 @@
 
 from dataclasses import dataclass
+from ..engine.game import Game
 from .asset import Asset
 
 @dataclass
 class Sprite(Asset):
-    ...
+    game: Game
+
+    def load(self) -> None:
+        self.game.send_tag(
+            'S_LOADSPRITE',
+            f'0:{self.index}'
+        )
+
+    def animate(self) -> None:
+        ...
