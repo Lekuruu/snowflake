48c08d845948f86265fe555b5da709d29d95f1a1
Lekuru <contact@lekuru.xyz>
2024-01-24 22:40:06 +0100

    Implement usage of `game.send_tag`

--- app/objects/gameobject.py
+++ app/objects/gameobject.py
@@ -1,6 +1,8 @@
 
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
+
+from .collections import SoundCollection
 from ..engine import Instance
 from .asset import Asset
 
@@ -15,25 +17,29 @@ class GameObject:
     x: int
     y: int
     game: "Game"
+    sounds: SoundCollection = SoundCollection()
 
     def initialize(self) -> None:
         blank_asset = Instance.assets.by_name('blank_png')
 
-        for client in self.game.clients:
-            client.send_tag(
-                'O_HERE',
-                self.id,
-                f'0:{blank_asset.index}',
-                self.x,
-                self.y,
-                0,  # TODO
-                1,  # TODO
-                0,  # TODO
-                0,  # TODO
-                0,  # TODO
-                self.name,
-                f'0:{blank_asset.index}',
-                0,  # TODO
-                1,  # TODO
-                0   # TODO
-            )
+        self.game.send_tag(
+            'O_HERE',
+            self.id,
+            f'0:{blank_asset.index}',
+            self.x,
+            self.y,
+            0,  # TODO
+            1,  # TODO
+            0,  # TODO
+            0,  # TODO
+            0,  # TODO
+            self.name,
+            f'0:{blank_asset.index}',
+            0,  # TODO
+            1,  # TODO
+            0   # TODO
+        )
+
+    def play_sound(self, sound_name: str) -> None:
+        sound = self.sounds.by_name(sound_name)
+        sound.play(self.game)
