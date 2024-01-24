991bcf4e8e2763d15745001acaf2dad221e975ef
Lekuru <contact@lekuru.xyz>
2024-01-24 22:45:07 +0100

    Add `from_index` & `from_name` class functions to sound

--- app/objects/sound.py
+++ app/objects/sound.py
@@ -1,7 +1,11 @@
 
 from ..engine.game import Game
+from ..engine import Instance
 from .asset import Asset
 
+from dataclasses import dataclass
+
+@dataclass
 class Sound(Asset):
     handle_id: int
     looping: bool = False
@@ -9,6 +13,56 @@ class Sound(Asset):
     radius: int = 0
     game_object_id: int = -1
     response_object_id: int = -1
+
+    @classmethod
+    def from_index(
+        cls,
+        index: int,
+        handle_id: int,
+        looping: bool = False,
+        volume: int = 100,
+        radius: int = 0,
+        game_object_id: int = -1,
+        response_object_id: int = -1
+    ) -> "Sound":
+        asset = Instance.sound_assets.by_index(index)
+
+        return Sound(
+            asset.index,
+            asset.name,
+            asset.url,
+            handle_id,
+            looping,
+            volume,
+            radius,
+            game_object_id,
+            response_object_id
+        )
+
+    @classmethod
+    def from_name(
+        cls,
+        name: str,
+        handle_id: int,
+        looping: bool = False,
+        volume: int = 100,
+        radius: int = 0,
+        game_object_id: int = -1,
+        response_object_id: int = -1
+    ) -> "Sound":
+        asset = Instance.sound_assets.by_name(name)
+
+        return Sound(
+            asset.index,
+            asset.name,
+            asset.url,
+            handle_id,
+            looping,
+            volume,
+            radius,
+            game_object_id,
+            response_object_id
+        )
 
     def play(self, game: Game) -> None:
         game.send_tag(
