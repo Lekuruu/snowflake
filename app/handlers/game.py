94e705a9656f6d144a7e29111de90e8d3af898bb
Lekuru <contact@lekuru.xyz>
2024-01-24 22:02:06 +0100

    Refactor /place_ready

--- app/handlers/game.py
+++ app/handlers/game.py
@@ -3,12 +3,12 @@ from ..engine import Instance, Penguin
 
 @Instance.events.register('/use')
 def use_handler(client: Penguin, object_id: int, x: int, y: int, local_x: float, local_y: float):
-    pass
+    ...
 
 @Instance.events.register('/anim_done')
 def on_animation_done(client: Penguin, param_1: int, param_2: int):
-    pass
+    ...
 
 @Instance.events.register('/intro_anim_done')
 def on_intro_done(client: Penguin):
-    pass
+    ...
--- app/handlers/playerselect.py
+++ app/handlers/playerselect.py
@@ -6,20 +6,12 @@ def ready_handler(client: Penguin):
     # Initialize window manager
     client.window_manager.load()
 
-    # Needs to be sent to show the player select
-    client.send_tag('P_MAPBLOCK', 't', 1, 1, 'iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAADklEQVQImWNghgEGIlkADWEAiDEh28IAAAAASUVORK5CYII=')
-    client.send_tag('P_MAPBLOCK', 'h', 1, 1, 'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAGCAAAAADfm1AaAAAADklEQVQImWOohwMG8pgA1rMdxRJRFewAAAAASUVORK5CYII=')
-
-    # Align player select to center
-    client.send_tag('UI_ALIGN', 101, 0, 0, 'center', 'scale_none')
-    client.send_tag('W_PLACE', 0, 1, 0)
-
+    # Intialize game
     client.initialize_game()
 
 @Instance.events.register('/place_ready')
 def on_place_ready(client: Penguin):
     # Setup camera
-    client.send_tag('O_HERE', 1, '0:1', 0, 0, 0, 1, 0, 0, 0, '', '0:1', 0, 1, 0)
     client.send_tag('P_CAMERA', 4.5, 2.49333, 0, 0, 1)
     client.send_tag('P_ZOOM', 1.000000)
 
