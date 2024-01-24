4a0537d5574b109d6351f67aa85c2a0e312c149e
Lekuru <contact@lekuru.xyz>
2024-01-24 22:27:40 +0100

    Add `initialize` function

--- app/objects/gameobject.py
+++ app/objects/gameobject.py
@@ -1,6 +1,11 @@
 
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
+from ..engine import Instance
 from .asset import Asset
+
+if TYPE_CHECKING:
+    from ..engine.game import Game
 
 @dataclass
 class GameObject:
@@ -9,3 +14,26 @@ class GameObject:
     asset: Asset
     x: int
     y: int
+    game: "Game"
+
+    def initialize(self) -> None:
+        blank_asset = Instance.assets.by_name('blank_png')
+
+        for client in self.game.clients:
+            client.send_tag(
+                'O_HERE',
+                self.id,
+                f'0:{blank_asset.index}',
+                self.x,
+                self.y,
+                0,  # TODO
+                1,  # TODO
+                0,  # TODO
+                0,  # TODO
+                0,  # TODO
+                self.name,
+                f'0:{blank_asset.index}',
+                0,  # TODO
+                1,  # TODO
+                0   # TODO
+            )
