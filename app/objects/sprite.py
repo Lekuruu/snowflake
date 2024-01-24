68dee94b6fd32dc1e86313bd332517a1f57f4b9a
Lekuru <contact@lekuru.xyz>
2024-01-24 23:10:56 +0100

    Add from_index & from_name functions

--- app/objects/sprite.py
+++ app/objects/sprite.py
@@ -1,11 +1,29 @@
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..engine.game import Game
+
 from dataclasses import dataclass
-from ..engine.game import Game
 from .asset import Asset
 
 @dataclass
 class Sprite(Asset):
-    game: Game
+    game: "Game"
+
+    @classmethod
+    def from_index(cls, index: int, game: "Game") -> "Sprite":
+        return cls(
+            index,
+            game
+        )
+
+    @classmethod
+    def from_name(cls, name: str, game: "Game") -> "Sprite":
+        return cls(
+            game.assets.by_name(name).index,
+            game
+        )
 
     def load(self) -> None:
         self.game.send_tag(
