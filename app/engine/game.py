c00eec1a3838ee63060f9543ca3d4f6df6674404
Lekuru <contact@lekuru.xyz>
2024-01-25 22:51:59 +0100

    Implement basic game object logic

--- app/engine/game.py
+++ app/engine/game.py
@@ -4,10 +4,11 @@ from typing import TYPE_CHECKING, List
 if TYPE_CHECKING:
     from .penguin import Penguin
 
-from app.objects.collections import ObjectCollection
+from app.objects.collections import ObjectCollection, AssetCollection
+from app.objects.ninjas import WaterNinja, SnowNinja, FireNinja
+from app.objects.enemies import Sly, Scrap, Tank, Tusk
 from app.objects.gameobject import GameObject
 from app.objects.sound import Sound
-from app.objects.asset import Asset
 
 import random
 import time
@@ -21,19 +22,30 @@ class Game:
         self.round = 1
         self.enemies = []
         self.map = random.randrange(1, 3)
+        self.bonus_cirteria = 'under_time' # TODO: Select random
+        self.game_start = time.time()
 
         self.objects = ObjectCollection()
 
     @property
-    def clients(self) -> list["Penguin"]:
+    def clients(self) -> List["Penguin"]:
         return [self.fire, self.snow, self.water]
 
     @property
-    def backgrounds(self) -> List[str]:
+    def backgrounds(self) -> List[GameObject]:
         return {
-            1: ['env_mountaintop_bg'],
-            2: ['forest_bg', 'forest_fg'],
-            3: ['cragvalley_bg', 'cragvalley_fg', 'crag_rock']
+            1: [
+                GameObject.from_asset('env_mountaintop_bg', self, x=4.5, y=-1.1)
+            ],
+            2: [
+                GameObject.from_asset('forest_bg', self, x=0, y=0),
+                GameObject.from_asset('forest_fg', self, x=0, y=1)
+            ],
+            3: [
+                GameObject.from_asset('cragvalley_bg', self, x=0, y=0),
+                GameObject.from_asset('cragvalley_fg', self, x=0, y=1),
+                GameObject.from_asset('crag_rock', self, x=0, y=2)
+            ]
         }[self.map]
 
     def start(self) -> None:
@@ -63,7 +75,36 @@ class Game:
         self.initialize_objects()
         self.wait_for_players()
 
-        # TODO: Game logic
+        # Play background music
+        Sound.from_name('mus_mg_201303_cjsnow_gamewindamb', looping=True).play(self)
+
+        # Place background sprites
+        for background in self.backgrounds:
+            obj = self.objects.by_name(background.name)
+            obj.place_sprite(background.name)
+
+        # TODO: Start animations
+
+        for client in self.clients:
+            # Close loading screen
+            player_select = client.window_manager.get_window('cardjitsu_snowplayerselect.swf')
+            player_select.send_action('closeCjsnowRoomToRoom')
+
+            # Close game button
+            close_button = client.window_manager.get_window('cardjitsu_snowclose.swf')
+            close_button.layer = 'bottomLayer'
+            close_button.load(
+                loadDescription="",
+                assetPath="",
+                xPercent=1,
+                yPercent=0
+            )
+
+        # Reset game time
+        self.game_start = time.time()
+
+        # Show round title
+        self.display_round_title()
 
     def send_tag(self, tag: str, *args) -> None:
         for player in self.clients:
@@ -77,5 +118,46 @@ class Game:
 
     def initialize_objects(self) -> None:
         """Initialize all game objects"""
-        # Play background music
-        Sound.from_name('mus_mg_201303_cjsnow_gamewindamb', looping=True).play(self)
+        # Background
+        for background in self.backgrounds:
+            self.objects.add(background)
+            background.place_object()
+
+        # Ninjas
+        self.objects.add(water := WaterNinja(self, x=0, y=0))
+        water.place_object()
+
+        self.objects.add(snow := SnowNinja(self, x=0, y=1))
+        snow.place_object()
+
+        self.objects.add(fire := FireNinja(self, x=0, y=2))
+        fire.place_object()
+
+        # Enemies
+        self.objects.add(sly := Sly(self, x=1, y=0))
+        sly.place_object()
+
+        self.objects.add(scrap := Scrap(self, x=1, y=1))
+        scrap.place_object()
+
+        self.objects.add(tank := Tank(self, x=1, y=2))
+        tank.place_object()
+
+        # Load sprites
+        for object in self.objects:
+            object.load_sprites()
+
+    def display_round_title(self) -> None:
+        # TODO: Implement other bonus criteria
+        for client in self.clients:
+            round_title = client.window_manager.get_window('cardjitsu_snowrounds.swf')
+            round_title.layer = 'bottomLayer'
+            round_title.load(
+                {
+                    'bonusCriteria': self.bonus_cirteria,
+                    'remainingTime': ((self.game_start + 300) - time.time()) * 1000,
+                    'roundNumber': self.round
+                },
+                loadDescription="",
+                assetPath=""
+            )
