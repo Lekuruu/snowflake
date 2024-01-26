3e36df5c5d7ce43c0e3e26d9af6023f2952e9d5b
Lekuru <contact@lekuru.xyz>
2024-01-26 16:27:21 +0100

    Remove "flaxp0000" from animation name

--- app/objects/enemies.py
+++ app/objects/enemies.py
@@ -18,13 +18,15 @@ class Sly(GameObject):
         Asset.from_name('sly_hit_anim'),
         Asset.from_name('sly_ko_anim'),
         Asset.from_name('sly_projectile_anim'),
-        Asset.from_name('sly_daze_anim')
+        Asset.from_name('sly_daze_anim'),
+        Asset.from_name('snowman_spawn_anim')
     }))
     sounds: SoundCollection = field(default_factory=lambda: SoundCollection({
         Sound.from_name('sfx_mg_2013_cjsnow_footstepsly_loop'),
         Sound.from_name('sfx_mg_2013_cjsnow_attacksly'),
         Sound.from_name('sfx_mg_2013_cjsnow_impactsly'),
-        Sound.from_name('sfx_mg_2013_cjsnow_snowmanslyhit')
+        Sound.from_name('sfx_mg_2013_cjsnow_snowmanslyhit'),
+        Sound.from_name('sfx_mg_2013_cjsnow_snowmenappear')
     }))
 
     def __eq__(self, other: "GameObject") -> bool:
@@ -47,13 +49,15 @@ class Scrap(GameObject):
         Asset.from_name('scrap_hit_anim'),
         Asset.from_name('scrap_move_anim'),
         Asset.from_name('scrap_ko_anim'),
-        Asset.from_name('scrap_dazed_anim')
+        Asset.from_name('scrap_dazed_anim'),
+        Asset.from_name('snowman_spawn_anim')
     }))
     sounds: SoundCollection = field(default_factory=lambda: SoundCollection({
         Sound.from_name('sfx_mg_2013_cjsnow_snowmanscraphit'),
         Sound.from_name('sfx_mg_2013_cjsnow_impactscrap'),
         Sound.from_name('sfx_mg_2013_cjsnow_footstepscrap_loop'),
-        Sound.from_name('sfx_mg_2013_cjsnow_attackscrap')
+        Sound.from_name('sfx_mg_2013_cjsnow_attackscrap'),
+        Sound.from_name('sfx_mg_2013_cjsnow_snowmenappear')
     }))
 
     def __eq__(self, other: "GameObject") -> bool:
@@ -68,17 +72,19 @@ class Tank(GameObject):
     assets: AssetCollection = field(default_factory=lambda: AssetCollection({
         Asset.from_name('tank_swipe_horiz_anim'),
         Asset.from_name('tank_swipe_vert_anim'),
-        Asset.from_name('tank_idle_anim_flaxp0000'),
-        Asset.from_name('tank_attack_anim_flaxp0000'),
+        Asset.from_name('tank_idle_anim'),
+        Asset.from_name('tank_attack_anim'),
         Asset.from_name('tank_hit_anim'),
         Asset.from_name('tank_move_anim'),
         Asset.from_name('tank_knockout_anim'),
-        Asset.from_name('tank_daze_anim')
+        Asset.from_name('tank_daze_anim'),
+        Asset.from_name('snowman_spawn_anim')
     }))
     sounds: SoundCollection = field(default_factory=lambda: SoundCollection({
         Sound.from_name('sfx_mg_2013_cjsnow_snowmantankhit'),
         Sound.from_name('sfx_mg_2013_cjsnow_footsteptank'),
-        Sound.from_name('sfx_mg_2013_cjsnow_attacktank')
+        Sound.from_name('sfx_mg_2013_cjsnow_attacktank'),
+        Sound.from_name('sfx_mg_2013_cjsnow_snowmenappear')
     }))
 
     def __eq__(self, other: "GameObject") -> bool:
@@ -100,7 +106,7 @@ class Tusk(GameObject):
         Asset.from_name('tusk_icicle_drop_anim'),
         Asset.from_name('tusk_stun_anim'),
         Asset.from_name('tusk_iciclesummon1_anim'),
-        Asset.from_name('tusk_iciclesummon2_anim'),
+        Asset.from_name('tusk_iciclesummon2_anim')
     }))
     sounds: SoundCollection = field(default_factory=lambda: SoundCollection({
         Sound.from_name('sfx_mg_2013_cjsnow_tusklaugh'),
