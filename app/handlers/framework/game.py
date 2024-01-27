41046db292a782abc6a02be6f646becafb3dfb11
Lekuru <contact@lekuru.xyz>
2024-01-27 14:25:14 +0100

    Fix `roomToRoomMinTime` event

--- app/handlers/framework/game.py
+++ app/handlers/framework/game.py
@@ -12,3 +12,7 @@ def payout_handler(client: Penguin, data: dict):
     client.logger.info(f'{client.name} left the game after payout.')
     client.send_to_room()
     client.disconnected = True
+
+@Instance.triggers.register('roomToRoomMinTime')
+def on_room_to_room_min_time(client: Penguin, data: dict):
+    client.is_ready = True
