67e94f829301ee27a107f89fb796405290e32f11
Lekuru <contact@lekuru.xyz>
2024-01-27 14:18:50 +0100

    Add `disconnected` flag when `quit` event was called

--- app/handlers/framework/game.py
+++ app/handlers/framework/game.py
@@ -5,21 +5,10 @@ from app.engine import Penguin, Instance
 def quit_handler(client: Penguin, data: dict):
     client.logger.info(f'{client.name} left the game.')
     client.send_to_room()
-    # TODO: Handle matchmaking
+    client.disconnected = True
 
 @Instance.triggers.register('quitFromPayout')
 def payout_handler(client: Penguin, data: dict):
     client.logger.info(f'{client.name} left the game after payout.')
     client.send_to_room()
-
-@Instance.triggers.register('roomToRoomComplete')
-def on_room_to_room_complete(client: Penguin, data: dict):
-    ...
-
-@Instance.triggers.register('roomToRoomMinTime')
-def on_room_to_room_min_time(client: Penguin, data: dict):
-    client.is_ready = True
-
-@Instance.triggers.register('roomToRoomScreenClosed')
-def on_room_to_room_screen_closed(client: Penguin, data: dict):
-    ...
+    client.disconnected = True
