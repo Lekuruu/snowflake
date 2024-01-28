af9908871ffb2ccc30142324a33d4955e62f8702
Lekuru <contact@lekuru.xyz>
2024-01-28 23:24:55 +0100

    Play confirm sound for all clients

--- app/handlers/framework/game.py
+++ app/handlers/framework/game.py
@@ -35,4 +35,4 @@ def on_confirm_clicked(client: Penguin, data: dict):
     client.is_ready = True
 
     confirm.add_sound('SFX_MG_2013_CJSnow_UIPlayerReady_VBR8')
-    confirm.play_sound('SFX_MG_2013_CJSnow_UIPlayerReady_VBR8', client)
+    confirm.play_sound('SFX_MG_2013_CJSnow_UIPlayerReady_VBR8')
