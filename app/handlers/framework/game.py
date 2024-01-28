3aa47eff0acb55559f00cced9b0123bd64f29d54
Lekuru <contact@lekuru.xyz>
2024-01-28 23:47:12 +0100

    Disable cards after confirming move

--- app/handlers/framework/game.py
+++ app/handlers/framework/game.py
@@ -36,3 +36,6 @@ def on_confirm_clicked(client: Penguin, data: dict):
 
     confirm.add_sound('SFX_MG_2013_CJSnow_UIPlayerReady_VBR8')
     confirm.play_sound('SFX_MG_2013_CJSnow_UIPlayerReady_VBR8')
+
+    snow_ui = client.window_manager.get_window('cardjitsu_snowui.swf')
+    snow_ui.send_payload('disableCards')
