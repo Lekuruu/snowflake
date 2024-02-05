d65f9d8fe621230d4563d665fe0439125791f65e
Lekuru <contact@lekuru.xyz>
2024-02-05 18:35:18 +0100

    Remove some default values from config

--- config.py
+++ config.py
@@ -5,18 +5,19 @@ import os
 load_dotenv(override=True)
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
-REDIS_DB = int(os.environ.get('REDIS_DB', 0))
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', '6379'))
+REDIS_DB = int(os.environ.get('REDIS_DB', '0'))
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
 
 POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
-POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', 5432))
-POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
-POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'postgres')
+POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', '5432'))
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
-PORT = int(os.environ.get('PORT', 7002))
+PORT = int(os.environ.get('PORT', '7002'))
 VERSION = os.environ.get('VERSION', 'FY15-20150206 (4954)r')
-MEDIA_LOCATION = os.environ.get('MEDIA_LOCATION', 'http://media.localhost')
+MEDIA_LOCATION = os.environ.get('MEDIA_LOCATION')
+
 ENABLE_DEBUG_PLAYERS = os.environ.get('ENABLE_DEBUG_PLAYERS', 'False').lower() == 'true'
 ENABLE_DEBUG_LOGGING = os.environ.get('ENABLE_DEBUG_LOGGING', 'False').lower() == 'true'
 DISABLE_POLICY_SERVER = os.environ.get('DISABLE_POLICY_SERVER', 'False').lower() == 'true'
