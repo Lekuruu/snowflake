305e76dd2d5a4fca0a2ed1472363c78ac6934180
Lekuru <contact@lekuru.xyz>
2024-02-05 18:15:47 +0100

    Refactor imports for database

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,4 +1,4 @@
 
 from .repositories import penguins, cards, stamps
-from .postgres import Postgres
+from .objects import Penguin, Card, Stamp
 from .constants import *
--- app/data/repositories/__init__.py
+++ app/data/repositories/__init__.py
@@ -1,2 +1,4 @@
 
 from . import penguins
+from . import stamps
+from . import cards
