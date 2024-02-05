1aaf1ed7ffb96c25d1fd285180e40a1a376fb6f1
Lekuru <contact@lekuru.xyz>
2024-02-05 18:15:19 +0100

    Add global session

---
+++ app/session.py
@@ -0,0 +1,27 @@
+
+from app.events import EventHandler, FrameworkHandler
+from app.objects import AssetCollection
+from app.data.postgres import Postgres
+from redis import Redis
+
+import config
+
+framework = FrameworkHandler()
+events = EventHandler()
+
+sound_assets = AssetCollection()
+assets = AssetCollection()
+
+database = Postgres(
+    config.POSTGRES_USER,
+    config.POSTGRES_PASSWORD,
+    config.POSTGRES_HOST,
+    config.POSTGRES_PORT
+)
+
+redis = Redis(
+    config.REDIS_HOST,
+    config.REDIS_PORT,
+    config.REDIS_DB,
+    config.REDIS_PASSWORD
+)
