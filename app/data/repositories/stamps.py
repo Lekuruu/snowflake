114059b44fdb10d26e2d715894cce9429133b99a
Lekuru <contact@lekuru.xyz>
2024-02-06 16:05:40 +0100

    Add `fetch_by_penguin_id` query

--- app/data/repositories/stamps.py
+++ app/data/repositories/stamps.py
@@ -2,18 +2,34 @@
 from __future__ import annotations
 
 from sqlalchemy.orm import Session
+from typing import List
 
+from ..objects import Stamp, PenguinStamp
 from .wrapper import session_wrapper
-from ..objects import Stamp
 
 @session_wrapper
-def fetch_one(id: int, session: Session | None = None):
+def fetch_one(id: int, session: Session | None = None) -> Stamp | None:
     return session.query(Stamp) \
         .filter(Stamp.id == id) \
         .first()
 
 @session_wrapper
-def fetch_all_by_group(group_id: int, session: Session | None = None):
+def fetch_all_by_group(group_id: int, session: Session | None = None) -> List[Stamp]:
     return session.query(Stamp) \
         .filter(Stamp.group_id == group_id) \
         .all()
+
+@session_wrapper
+def fetch_by_penguin_id(
+    penguin_id: int,
+    group_id: int | None = None,
+    session: Session | None = None
+) -> List[Stamp]:
+    query = session.query(Stamp) \
+        .join(PenguinStamp, Stamp.id == PenguinStamp.stamp_id) \
+        .filter(PenguinStamp.penguin_id == penguin_id)
+
+    if group_id:
+        query = query.filter(Stamp.group_id == group_id)
+
+    return query.all()
