77feaccfa0f069ca54e8980889c32a34b8bad316
Lekuru <contact@lekuru.xyz>
2024-02-06 17:59:12 +0100

    Fix dataclasses for `SnowLobby`

--- app/engine/place.py
+++ app/engine/place.py
@@ -1,10 +1,13 @@
 
 from app.protocols.metaplace.places import Place, MapBlocks, Camera
+from dataclasses import dataclass
 
+@dataclass
 class SnowMapBlocks(MapBlocks):
     tilemap: str = "iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAADklEQVQImWNghgEGIlkADWEAiDEh28IAAAAASUVORK5CYII="
     heightmap: str = "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAGCAAAAADfm1AaAAAADklEQVQImWOohwMG8pgA1rMdxRJRFewAAAAASUVORK5CYII="
 
+@dataclass
 class SnowCamera(Camera):
     position: tuple = (4.5, 2.5, 0)
     lock_zoom: bool = True
@@ -13,6 +16,7 @@ class SnowCamera(Camera):
     tile_size: int = 100
     elevation_scale: float = 0.031250
 
+@dataclass
 class SnowLobby(Place):
     id: int = 0
     name: str = "snow_lobby"
