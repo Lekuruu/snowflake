16c2d3182f457fec7fb61b9ca2a696664147f067
Lekuru <contact@lekuru.xyz>
2024-02-07 18:43:17 +0100

    Implement card handlers

--- app/handlers/game/__init__.py
+++ app/handlers/game/__init__.py
@@ -1,7 +1,7 @@
 
 from . import callbacks
 from . import confirm
+from . import cards
 from . import rooms
 from . import quit
 from . import use
-from . import ui
---
+++ app/handlers/game/cards.py
@@ -0,0 +1,61 @@
+
+from app.engine import Penguin
+from app.data import TipPhase
+from app import session
+
+@session.framework.register('RewardCardResponse')
+def on_card_loaded(client: Penguin, data: dict):
+    ...
+
+@session.framework.register('cardClick')
+def on_card_clicked(client: Penguin, data: dict):
+    element = data['element']
+    value = data['value']
+    id = data['cardId']
+
+    if not (card := client.get_power_card(id)):
+        return
+
+    if card.value != value:
+        return
+
+    if card.element != element:
+        return
+
+    client.selected_card = card
+    client.game.grid.change_tile_sprites_for_client(client, 'ui_tile_attack')
+
+@session.framework.register('unselectCard')
+def on_card_deselect(client: Penguin, data: dict):
+    if not client.selected_card:
+        return
+
+    client.selected_card = None
+    client.game.grid.change_tile_sprites_for_client(client, 'ui_tile_move')
+
+@session.framework.register('cardCount')
+def on_card_count(client: Penguin, data: dict):
+    card_amount = data['numCards']
+    # TODO
+
+@session.framework.register('ShowMemberCardInfoTip')
+def on_membercard_info_clicked(client: Penguin, data: dict):
+    if client.last_tip == TipPhase.MEMBER_CARD:
+        client.hide_tip()
+        return
+
+    client.send_tip(TipPhase.MEMBER_CARD)
+
+@session.framework.register('memberCardClick')
+def on_membercard_select(client: Penguin, data: dict):
+    if not client.is_member:
+        return
+
+    # TODO
+
+@session.framework.register('unselectMemberCard')
+def on_membercard_deselect(client: Penguin, data: dict):
+    if not client.is_member:
+        return
+
+    # TODO
