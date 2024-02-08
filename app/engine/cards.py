e3bfc59d32a86505e530fd2c32e558135f8104a7
Lekuru <contact@lekuru.xyz>
2024-02-08 20:07:21 +0100

    Implement card pattern sprite

--- app/engine/cards.py
+++ app/engine/cards.py
@@ -8,9 +8,16 @@ from app.data import Card
 class CardObject(Card):
     def __init__(self, card: Card, game: Game) -> None:
         self.__dict__.update(card.__dict__)
+        self.game = game
         self.object = GameObject(
             game,
             card.name,
+            x_offset=0.5,
+            y_offset=1
+        )
+        self.pattern = GameObject(
+            game,
+            'ui_card_pattern',
             x_offset=0.5,
             y_offset=1
         )
@@ -19,6 +26,10 @@ class CardObject(Card):
         return f'<CardObject {self.id} ({self.x}, {self.y})>'
 
     def place(self, x: int, y: int) -> None:
+        self.place_card_sprite(x, y)
+        self.place_pattern_sprite(x, y)
+
+    def place_card_sprite(self, x: int, y: int) -> None:
         self.object.x = x
         self.object.y = y
         self.object.place_object()
@@ -28,5 +39,43 @@ class CardObject(Card):
             's': 'ui_card_snow',
         }[self.element])
 
+    def place_pattern_sprite(self, x: int, y: int) -> None:
+        self.pattern.x_offset = 0.5
+        self.pattern.y_offset = 1
+
+        max_x = max(*self.game.grid.x_range)
+        min_x = min(*self.game.grid.x_range)
+        max_y = max(*self.game.grid.y_range)
+        min_y = min(*self.game.grid.y_range)
+
+        # Get surrounding tiles of card
+        x_range = range(x - 1, x + 2)
+        y_range = range(y - 1, y + 2)
+
+        # If the card is on the edge of the grid, remove the out-of-bounds tiles
+        if y == min_y:
+            y_range = y_range[1:]
+            self.pattern.y_offset = 1
+
+        if y == max_y:
+            y_range = y_range[:-1]
+            self.pattern.y_offset = 0
+
+        if x == min_x:
+            x_range = x_range[1:]
+            self.pattern.x_offset = 1
+
+        if x == max_x:
+            x_range = x_range[:-1]
+            self.pattern.x_offset = 0
+
+        pattern = f'{len(x_range)}x{len(y_range)}'
+
+        self.pattern.x = x
+        self.pattern.y = y
+        self.pattern.place_object()
+        self.pattern.place_sprite(f'ui_card_pattern{pattern}')
+
     def remove(self) -> None:
         self.object.remove_object()
+        self.pattern.remove_object()
