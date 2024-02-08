2f77db05a1de2971193a26980a72783a5f69da03
Lekuru <contact@lekuru.xyz>
2024-02-08 18:49:35 +0100

    Implement `place` & `remove` methods

--- app/engine/cards.py
+++ app/engine/cards.py
@@ -1,4 +1,6 @@
 
+from __future__ import annotations
+
 from app.objects import GameObject
 from app.engine.game import Game
 from app.data import Card
@@ -6,26 +8,25 @@ from app.data import Card
 class CardObject(Card):
     def __init__(self, card: Card, game: Game) -> None:
         self.__dict__.update(card.__dict__)
-        self.object = GameObject(game, card.name)
+        self.object = GameObject(
+            game,
+            card.name,
+            x_offset=0.5,
+            y_offset=1
+        )
 
     def __repr__(self) -> str:
         return f'<CardObject {self.id} ({self.x}, {self.y})>'
 
-    @property
-    def x(self):
-        return self.object.x
+    def place(self, x: int, y: int) -> None:
+        self.object.x = x
+        self.object.y = y
+        self.object.place_object()
+        self.object.place_sprite({
+            'f': 'ui_card_fire',
+            'w': 'ui_card_water',
+            's': 'ui_card_snow',
+        }[self.element])
 
-    @x.setter
-    def x(self, value):
-        self.object.x = value
-
-    @property
-    def y(self):
-        return self.object.y
-
-    @y.setter
-    def y(self, value):
-        self.object.y = value
-
-    def place(self) -> None:
-        ...
+    def remove(self) -> None:
+        self.object.remove_object()
