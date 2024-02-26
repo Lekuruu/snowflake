40fdcbbb177480d6e4b9df9dbbfad9571a692a71
Lekuru <contact@lekuru.xyz>
2024-02-27 00:00:08 +0100

    Remove useless `updateStamina` event after game start

--- app/engine/game.py
+++ app/engine/game.py
@@ -173,7 +173,6 @@ class Game:
                 continue
 
             snow_ui = client.get_window('cardjitsu_snowui.swf')
-            snow_ui.send_payload('updateStamina', {'cardData': None, 'cycle': False, 'stamina': 0})
             snow_ui.send_payload('noCards')
 
         # Run game loop until game ends
