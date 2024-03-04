83dd5393a02582cd863329855fc7a17ec5feb9c3
Lekuru <contact@lekuru.xyz>
2024-03-04 16:57:30 +0100

    Fix "Heal 15" stamp

--- app/engine/game.py
+++ app/engine/game.py
@@ -638,9 +638,6 @@ class Game:
                 if isinstance(target, Ninja):
                     ninja.heal_target(target)
 
-                    if ninja.name == 'Snow':
-                        ninja.heals += 1
-
                 if ninja.heals >= 15:
                     # Unlock "Heal 15" stamp
                     self.unlock_stamp(477)
