8b67f31e945c15fa33f1c17cb0aa6fb41d09be28
Lekuru <contact@lekuru.xyz>
2024-09-17 14:14:37 +0200

    Cleanup imports

--- app/engine/game.py
+++ app/engine/game.py
@@ -1,6 +1,5 @@
 
 from __future__ import annotations
-
 from typing import TYPE_CHECKING, Callable, List
 
 if TYPE_CHECKING:
@@ -9,11 +8,8 @@ if TYPE_CHECKING:
 from app.data.repositories import stamps, penguins, items
 from app.data import (
     ExpRequirements,
-    InputModifier,
-    InputTarget,
     SnowRewards,
     MirrorMode,
-    InputType,
     TipPhase
 )
 
