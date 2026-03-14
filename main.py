bad934d98d4d6be53e2c796a4fdfc5bf8f98b2b1
Lekuru <contact@lekuru.xyz>
2026-03-14 17:58:45 +0100

    Refactor & cleanup launch script

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ logging.basicConfig(
     handlers=[Console],
     level=logging.DEBUG if config.ENABLE_DEBUG_LOGGING else logging.INFO
 )
+world_server: SnowflakeWorld
+policy_server: SocketPolicyServer
 
 def on_shutdown(*args):
     """Kick all players from the server, before the reactor stops"""
@@ -24,29 +26,33 @@ def on_shutdown(*args):
 
     reactor.callLater(0.1, reactor.stop)
 
-signal.signal(signal.SIGINT, on_shutdown)
-
-if __name__ == "__main__":
+def main():
     global world_server, policy_server
 
+    world_server = SnowflakeWorld()
+    world_server.listen(config.PORT)
+
+    if config.ENABLE_POLICY_SERVER:
+        policy_server = SocketPolicyServer(
+            config.POLICY_DOMAIN,
+            config.POLICY_PORT
+        )
+        policy_server.listen(843)
+
+    if config.WEBSOCKET_ENABLED:
+        # Use txws to wrap tcp factory
+        ws_factory = WebSocketWrapper(world_server)
+        ws_factory.listen(config.WEBSOCKET_PORT)
+
+    reactor.run()
+
+if __name__ == "__main__":
+    # Handle shutdown signals to kick players before the reactor stops
+    signal.signal(signal.SIGINT, on_shutdown)
+
     try:
-        world_server = SnowflakeWorld()
-        world_server.listen(config.PORT)
-
-        if config.ENABLE_POLICY_SERVER:
-            policy_server = SocketPolicyServer(
-                config.POLICY_DOMAIN,
-                config.POLICY_PORT
-            )
-            policy_server.listen(843)
-
-        if config.WEBSOCKET_ENABLED:
-            # Use txws to wrap tcp factory
-            ws_factory = WebSocketWrapper(world_server)
-            ws_factory.listen(config.WEBSOCKET_PORT)
-
-        reactor.run()
+        main()
     except Exception as e:
         traceback.print_exc()
-        logging.fatal(f'Failed to start server: {e}')
+        logging.fatal(f'An error error occurred: {e}')
         exit(1)
