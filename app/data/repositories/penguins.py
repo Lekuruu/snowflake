27b86550b9c26ba8bebdc6fc2e30deac34b8cc36
Lekuru <contact@lekuru.xyz>
2026-04-07 11:14:44 +0200

    Fix type hints in repository system

--- app/data/repositories/penguins.py
+++ app/data/repositories/penguins.py
@@ -3,29 +3,29 @@ from __future__ import annotations
 
 from sqlalchemy.orm import Session
 
-from .wrapper import session_wrapper
+from .wrapper import SessionProvider, session_wrapper
 from ..objects import Penguin
 
 @session_wrapper
-def fetch_by_id(id: int, session: Session = ...) -> Penguin | None:
+def fetch_by_id(id: int, session: Session = SessionProvider) -> Penguin | None:
     return session.query(Penguin) \
         .filter(Penguin.id == id) \
         .first()
 
 @session_wrapper
-def fetch_by_name(name: str, session: Session = ...) -> Penguin | None:
+def fetch_by_name(name: str, session: Session = SessionProvider) -> Penguin | None:
     return session.query(Penguin) \
         .filter(Penguin.username == name) \
         .first()
 
 @session_wrapper
-def fetch_by_nickname(nickname: str, session: Session = ...) -> Penguin | None:
+def fetch_by_nickname(nickname: str, session: Session = SessionProvider) -> Penguin | None:
     return session.query(Penguin) \
         .filter(Penguin.nickname == nickname) \
         .first()
 
 @session_wrapper
-def update(id: int, updates: dict, session: Session = ...) -> int:
+def update(id: int, updates: dict, session: Session = SessionProvider) -> int:
     rows = session.query(Penguin) \
         .filter(Penguin.id == id) \
         .update(updates)
